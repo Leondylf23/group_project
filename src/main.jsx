c1ad3230e6cab1bd6486ecffb1c0b5ee340c35ef
Fahmi <itsfahmiefendy@gmail.com>
2024-01-19 11:26:34 +0700

    feat: create detail page

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Routes, Route, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import CreatePage from './pages/Create';
-import Home from './pages/Home';
-import NavBar from './components/Navbar';
-import { MainContext } from './components/MainContext';
+import Home from "./pages/Home";
+import CreatePage from "./pages/Create";
+import NavBar from "./components/Navbar";
+import { MainContext } from "./components/MainContext";
 
-import './index.css';
-import LoadingContainer from './components/LoadingContainer';
+import "./index.css";
+import Detail from "./pages/Detail";
+import LoadingContainer from "./components/LoadingContainer";
 
 // const router = createBrowserRouter([
 //   {
@@ -21,23 +22,23 @@ import LoadingContainer from './components/LoadingContainer';
 //   },
 // ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MainContext>
       <BrowserRouter>
         <NavBar />
-        <Suspense fallback={<LoadingContainer isFullHeight={true}/>}>
+        <Suspense fallback={<LoadingContainer isFullHeight={true} />}>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/create' element={<CreatePage />} />
-            <Route path='/work' element={<Home category={"work"} />} />
-            <Route path='/family' element={<Home category={"family"} />} />
-            <Route path='/personal' element={<Home category={"personal"} />} />
-            <Route path='/:id' element={<CreatePage isFromDetail />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/work" element={<Home category={"work"} />} />
+            <Route path="/family" element={<Home category={"family"} />} />
+            <Route path="/personal" element={<Home category={"personal"} />} />
+            <Route path="/:id" element={<Detail />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
       {/* <RouterProvider router={router} fallbackElement={<LoadingContainer isFullHeight={true} />} /> */}
     </MainContext>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
